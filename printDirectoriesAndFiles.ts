5a5f799ea27fa43d64f8d1745ba9405f0a4bdb40
nare <nare@preezma.com>
2024-08-20 22:37:00 +0400

    read and write files recursively with indentation

--- printDirectoriesAndFiles.ts
+++ printDirectoriesAndFiles.ts
@@ -1,13 +1,28 @@
 import * as path from "path";
-import * as fs from 'fs/promises';
+import * as fs from "fs/promises";
 
 const filePathPrint = path.join(__dirname, "txt/print.txt");
-const startPath = path.resolve('node_modules')
+const startPath = path.resolve("node_modules/diff");
 
-const readDirectory = async (dirPath: string): Promise<void> => {
-  const data = await fs.readdir(dirPath, { recursive: true });
-  console.log(data)
+const writeNameInFile = async (name: string): Promise<void> => {
+  fs.appendFile(filePathPrint, name);
 };
 
+const indent = (count: number) => "  ".repeat(count);
+
+const readDirectory = async (dirPath: string, space = 0): Promise<void> => {
+  const directories = await fs.readdir(dirPath, {
+    withFileTypes: true,
+  });
+
+  for (const directory of directories) {
+    const fullPath = path.join(dirPath, directory.name);
+    const tabs = indent(space);
+    await writeNameInFile(`${tabs}${directory.name}/\n`);
+    if (directory.isDirectory()) {
+      await readDirectory(fullPath, space + 1);
+    }
+  }
+};
 
 readDirectory(startPath);
